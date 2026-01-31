c0c292b3049d7d1ad02845bb8bdf32e59f7e2eb8
KaelDucatti <ducatti860@gmail.com>
2026-01-31 16:45:21 -0300

    test(section01): add test for getters and setters

--- section01/structs.go
+++ section01/structs.go
@@ -3,25 +3,74 @@ package section01
 import "errors"
 
 type Person struct {
-	Wage	float32
-	Name	string
-	Age		int
+	wage	float32
+	name	string
+	age		int
 }
 
 func NewPerson(wage float32, name string, age int) (*Person, error) {
-	if wage <= 0 {
-		return nil, errors.New("Wage must be greater than 0")
+	if err := validateWage(wage); err != nil {
+		return nil, err
 	}
-	if name == "" {
-		return nil, errors.New("Name cannot be void")
+	if err := validateName(name); err != nil {
+		return nil, err
 	}
-	if age <= 0 {
-		return nil, errors.New("Age must be greater than 0")
+	if err := validateAge(age); err != nil {
+		return nil, err
 	}
 
 	return &Person{
-		Name: name,
-		Age: age,
-		Wage: wage,
+		wage: wage,
+		name: name,
+		age: age,
 	}, nil
+}
+
+func (p *Person) Wage() float32 { return p.wage }
+func (p *Person) Name() string { return p.name }
+func (p *Person) Age() int { return p.age }
+
+func (p *Person) SetWage(wage float32) error {
+	if err := validateWage(wage); err != nil {
+		return err
+	}
+	p.wage = wage
+	return nil
+}
+
+func (p *Person) SetName(name string) error {
+	if err := validateName(name); err != nil {
+		return err
+	}
+	p.name = name
+	return nil
+}
+
+func (p *Person) SetAge(age int) error {
+	if err := validateAge(age); err != nil {
+		return err
+	}
+	p.age = age
+	return nil
+}
+
+func validateWage(wage float32) error {
+	if wage < 0 {
+		return errors.New("Wage cannot be less than 0")
+	}
+	return nil
+}
+
+func validateName(name string) error {
+	if name == "" {
+		return errors.New("Name cannot be void")
+	}
+	return nil
+}
+
+func validateAge(age int) error {
+	if age <= 0 {
+		return errors.New("Age must be greater than 0")
+	}
+	return nil
 }
--- section01/structs_test.go
+++ section01/structs_test.go
@@ -7,51 +7,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestStruct(t *testing.T) {
+func TestNewPerson(t *testing.T) {
+	t.Run("Success Cases", func (t *testing.T) {
+		t.Run("should return Person instance", func(t *testing.T) {
+			require := require.New(t)
+
+			p, err := NewPerson(4800, "Kael", 26)
+
+			require.NoError(err)
+			require.Equal(float32(4800), p.Wage())
+			require.Equal("Kael", p.Name())
+			require.Equal(26, p.Age())
+		})
+	})
 	t.Run("Validation Errors", func(t *testing.T) {
-		t.Run("should return error when wage is equal to or less than zero", func (t *testing.T) {
+		t.Run("should return wage error", func(t *testing.T) {
 			require := require.New(t)
-	
-			testCases := []struct {
-				testName 	string
-				wage		float32
-				name		string
-				age			int
-			}{
-				{"wage is zero", 0, "Kael", 26},
-				{"wage is negative", -1, "Kael", 26},
-			}
-			
-			for _, test := range testCases {
-				t.Run(test.testName, func(t *testing.T) {
-					_, err := NewPerson(test.wage, test.name, test.age)
-					require.Error(err)
-					require.EqualError(err, "Wage must be greater than 0")
-				})
-			}
+
+			_, err := NewPerson(-1, "Kael", 26)
+
+			require.Error(err)
+			require.EqualError(err, "Wage cannot be less than 0")
 		})
-		t.Run("should return error when age is equal to or less than zero", func (t *testing.T) {
-			require := require.New(t)
-	
-			testCases := []struct {
-				testName 	string
-				wage		float32
-				name		string
-				age			int
-			}{
-				{"Age is zero", 4800, "Kael", 0},
-				{"Age is negative", 4800, "Kael", -1},
-			}
-			
-			for _, test := range testCases {
-				t.Run(test.testName, func(t *testing.T) {
-					_, err := NewPerson(test.wage, test.name, test.age)
-					require.Error(err)
-					require.EqualError(err, "Age must be greater than 0")
-				})
-			}
-		})
-		t.Run("should return error when name is void", func (t *testing.T) {
+		t.Run("should return name error", func(t *testing.T) {
 			require := require.New(t)
 
 			_, err := NewPerson(4800, "", 26)
@@ -59,7 +37,113 @@ func TestStruct(t *testing.T) {
 			require.Error(err)
 			require.EqualError(err, "Name cannot be void")
 		})
+		t.Run("should return age error", func(t *testing.T) {
+			require := require.New(t)
+
+			testCases := []struct {
+				testName	string
+				wage		float32
+				name		string
+				age			int
+			}{
+				{"age is negative", 4800, "Kael", -1},
+				{"age is zero", 4800, "Kael", -0},
+			}
+
+			for _, test := range testCases {
+				t.Run(test.testName, func(t *testing.T) {
+					_, err := NewPerson(test.wage, test.name, test.age)
+
+					require.Error(err)
+					require.EqualError(err, "Age must be greater than 0")
+				})
+			}
+		})
 	})
+}
+
+func TestSetters(t *testing.T) {
+	t.Run("Success Case", func(t *testing.T) {
+		t.Run("should update wage and return nil", func(t *testing.T) {
+			require := require.New(t)
+			p, _ := NewPerson(4800, "Kael", 26)
+			
+			err := p.SetWage(5200)
+
+			require.NoError(err)
+			require.Equal(float32(5200), p.Wage())
+		})
+		t.Run("should update name and return nil", func(t *testing.T) {
+			require := require.New(t)
+			p, _ := NewPerson(4800, "Kael", 26)
+			
+			err := p.SetName("Mikael")
+	
+			require.NoError(err)
+			require.Equal("Mikael", p.Name())
+		})
+		t.Run("should update age and return nil", func(t *testing.T) {
+			require := require.New(t)
+			p, _ := NewPerson(4800, "Kael", 26)
+
+			err := p.SetAge(27)
+
+			require.NoError(err)
+			require.Equal(int(27), p.Age())
+		})
+	})
+	t.Run("Error Validation", func(t *testing.T) {
+		t.Run("should return wage error", func(t *testing.T) {
+			require := require.New(t)
+			p, _ := NewPerson(4800, "Kael", 26)
+
+			err := p.SetWage(-1)
+
+			require.Error(err)
+			require.EqualError(err, "Wage cannot be less than 0")
+			require.Equal(float32(4800), p.Wage())
+		})
+		t.Run("should return name error", func(t *testing.T) {
+			require := require.New(t)
+			p, _ := NewPerson(4800, "Kael", 26)
+
+			err := p.SetName("")
+
+			require.Error(err)
+			require.EqualError(err, "Name cannot be void")
+			require.Equal("Kael", p.Name())
+		})
+		t.Run("should return age error", func(t *testing.T) {
+			require := require.New(t)
+			p, _ := NewPerson(4800, "Kael", 26)
+
+			testCases := []struct {
+				testName 	string
+				Age			int
+			}{
+				{"age is zero", 0},
+				{"age is negative", -1},
+			}
+
+			for _, test := range testCases {
+				t.Run(test.testName, func(t *testing.T) {
+					err := p.SetAge(test.Age)
+					require.Error(err)
+					require.EqualError(err, "Age must be greater than 0")
+					require.Equal(int(26), p.Age())
+				})
+			}
+		})
+	})
+}
+
+func TestGetters(t *testing.T) {
+	require := require.New(t)
+	p, _ := NewPerson(4800, "Kael", 26)
+
+	require.Equal(p.Wage(), float32(4800))
+	require.Equal(p.Name(), "Kael")
+	require.Equal(p.Age(), 26)
 }
 
 func ExampleNewPerson() {
